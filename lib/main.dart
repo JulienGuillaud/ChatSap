ce77c12cc01e5fc2bf2c019e617961db1e5528bc
Enzo Foucaud <foucauden@gmail.com>
2022-09-13 15:57:26 +0200

    New: Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:chat_sap/screens/welcome/welcome_screen.dart';
 import 'package:chat_sap/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // init Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // run app
   runApp(MyApp());
 }
 
